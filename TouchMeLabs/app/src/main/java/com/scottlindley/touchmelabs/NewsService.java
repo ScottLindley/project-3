e6ff4fe07a48c32353e4a47ede3dfe3db90acc66
ScottLindley <scottdlindley@gmail.com>
2016-12-01 09:31:24 -0500

    Added function to broadcast intent to LocalBroadcastManager

--- TouchMeLabs/app/src/main/java/com/scottlindley/touchmelabs/NewsService.java
+++ TouchMeLabs/app/src/main/java/com/scottlindley/touchmelabs/NewsService.java
@@ -2,6 +2,8 @@ package com.scottlindley.touchmelabs;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +14,8 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static android.R.id.list;
+
 
 /**
  * Created by Scott Lindley on 11/30/2016.
@@ -19,20 +23,26 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsService extends JobService implements NewsXmlParser.ParseFinishedListener{
     private static final String mBaseURL = "http://api.smmry.com";
+    private JobParameters mJobParameters;
     private NewsXmlParser mParser;
     private List<String> mStoryLinks;
     private List<GsonStory> mGsonStories;
-    private static List<Story> mStories;
+    private static List<NewsStory> mStories;
+    private String[] mTitles;
+    private String[] mSummary;
+    private String[] mLinks;
     private int mFailedResponses = 0;
 
     @Override
     public boolean onStartJob(final JobParameters jobParameters) {
         mGsonStories = new ArrayList<GsonStory>();
-        mStories = new ArrayList<Story>();
+        mStories = new ArrayList<NewsStory>();
+        mJobParameters = jobParameters;
 
         //See NewsXmlParser class
         mParser = new NewsXmlParser(this);
         mParser.getXML();
+        return false;
     }
 
     @Override
@@ -46,12 +56,12 @@ public class NewsService extends JobService implements NewsXmlParser.ParseFinish
         mStoryLinks = mParser.getStoryLinks();
         //Make a Smmry api call for each story link
         for (String link : mStoryLinks){
-            makeRetroFitCall(link);
+            makeRetroFitCall(link, mJobParameters);
         }
     }
 
     //Makes a call using retrofit with a single story url
-    private void makeRetroFitCall(final String link){
+    private void makeRetroFitCall(final String link, final JobParameters jobParameters){
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(mBaseURL)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -66,15 +76,25 @@ public class NewsService extends JobService implements NewsXmlParser.ParseFinish
                     //Takes the GsonStory response and adds it to mStories
                     GsonStory gsonStory = (response.body());
                     mGsonStories.add(gsonStory);
-                    mStories.add(new Story(
+                    mStories.add(new NewsStory(
                             gsonStory.getTitle(),
                             gsonStory.getContent(),
                             link
                     ));
                     //This checks if all links have been run through a retrofit call (successful or not)
                     if (mGsonStories.size() + mFailedResponses == mStoryLinks.size()) {
-                        //TODO: call DBHelper method here to send completed list of stories
-                        ContentDBHelper.getInstance().fetchNewStories();
+                        if (mFailedResponses == 0) {
+                            convertStoryToStringArrays();
+
+                            Intent intent = new Intent("service intent");
+                            intent.putExtra("titles", mTitles);
+                            intent.putExtra("summaries", mSummary);
+                            intent.putExtra("links", mLinks);
+                            intent.putExtra("service name", "news service");
+
+                            LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
+                            jobFinished(jobParameters, false);
+                        }
                     }
                 }
             }
@@ -86,14 +106,18 @@ public class NewsService extends JobService implements NewsXmlParser.ParseFinish
                 //This checks if all links have been run through a retrofit call (successful or not)
                 if(mGsonStories.size()+mFailedResponses == mStoryLinks.size()){
                     //TODO: call DBHelper method here to send completed list of stories
-                    ContentDBHelper.getInstance().fetchNewStories();
                 }
             }
 
         });
     }
 
-    public static List<Story> getStories (){
-        return mStories;
+    public void convertStoryToStringArrays(){
+        for (int i=0; i<mStories.size(); i++){
+            mTitles[i] = mStories.get(i).getTitle();
+            mSummary[i] = mStories.get(i).getContent();
+            mLinks[i] = mStories.get(i).getUrl();
+        }
     }
+
 }
