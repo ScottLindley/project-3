b1823ae79e93b06e695472b6afdb267d1be7ad91
Jon Lieblich <jlieblich1@gmail.com>
2016-12-01 16:30:50 -0500

    set up classes

---
+++ TouchMeLabs/app/src/main/java/com/scottlindley/touchmelabs/RecyclerViewComponents/CardRecyclerViewAdapter.java
@@ -0,0 +1,25 @@
+package com.scottlindley.touchmelabs.RecyclerViewComponents;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by jonlieblich on 12/1/16.
+ */
+
+public class CardRecyclerViewAdapter extends RecyclerView.Adapter<CardContentViewHolder> {
+    @Override
+    public CardContentViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(CardContentViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ TouchMeLabs/app/src/main/java/com/scottlindley/touchmelabs/RecyclerViewComponents/NewsStoryViewHolder.java
@@ -0,0 +1,13 @@
+package com.scottlindley.touchmelabs.RecyclerViewComponents;
+
+import android.view.View;
+
+/**
+ * ViewHolder for {@link NewsStory} card.
+ */
+
+public class NewsStoryViewHolder extends CardContentViewHolder{
+    public NewsStoryViewHolder(View itemView) {
+        super(itemView);
+    }
+}
