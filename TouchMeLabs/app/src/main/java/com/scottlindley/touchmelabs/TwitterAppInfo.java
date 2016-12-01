d632c978c4c7043bf750c9aaa14cf9f50d6151d9
ScottLindley <scottdlindley@gmail.com>
2016-12-01 10:13:17 -0500

    I think I finished? Can I has bearer token?

---
+++ TouchMeLabs/app/src/main/java/com/scottlindley/touchmelabs/TwitterAppInfo.java
@@ -0,0 +1,11 @@
+package com.scottlindley.touchmelabs;
+
+/**
+ * Created by Scott Lindley on 12/1/2016.
+ */
+
+public class TwitterAppInfo {
+    public static final String CONSUMER_KEY = "AWCimXwyBSLKABzIikoalhsSo";
+    public static final String CONSUMER_SECRET = "W5kVTHGQmlFbSWWqP3r9N7OLlq5rSlItegmGwoe11yX0BBmEPg";
+    public static final String BASE_URL = "https://api.twitter.com/1.1/statuses/home_timeline.json";
+}
