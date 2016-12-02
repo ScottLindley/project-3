0db29b97080f768891b4be38021934eb4c5aabf6
ScottLindley <scottdlindley@gmail.com>
2016-12-01 19:28:03 -0500

    completely changed OAuth, please pull and run on device

--- TouchMeLabs/app/src/main/java/com/scottlindley/touchmelabs/AlertDialogManager.java
+++
@@ -1,8 +0,0 @@
-package com.scottlindley.touchmelabs;
-
-/**
- * Created by Scott Lindley on 12/1/2016.
- */
-
-public class AlertDialogManager {
-}
--- TouchMeLabs/app/src/main/java/com/scottlindley/touchmelabs/TwitterAppInfo.java
+++ TouchMeLabs/app/src/main/java/com/scottlindley/touchmelabs/TwitterAppInfo.java
@@ -7,5 +7,5 @@ package com.scottlindley.touchmelabs;
 public class TwitterAppInfo {
     public static final String CONSUMER_KEY = "AWCimXwyBSLKABzIikoalhsSo";
     public static final String CONSUMER_SECRET = "W5kVTHGQmlFbSWWqP3r9N7OLlq5rSlItegmGwoe11yX0BBmEPg";
-    public static final String BASE_URL = "https://api.twitter.com/1.1/statuses/home_timeline.json";
+    public static final String CALLBACK_URL = "http://www.touchlab.co/#intro";
 }
