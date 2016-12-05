70825ca482a21f9317f33664b416e890e4991725
Jon Lieblich <jlieblich1@gmail.com>
2016-12-05 17:29:33 -0500

    this wont work

--- TouchMeLabs/app/src/main/java/com/scottlindley/touchmelabs/MainActivity.java
+++ TouchMeLabs/app/src/main/java/com/scottlindley/touchmelabs/MainActivity.java
@@ -189,7 +189,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
+                                           @NonNull final int[] grantResults) {
+        mListener = new OnLocationPermissionResponseListener() {
+            @Override
+            public void setPermissionResponseListener(int response) {
+                response = grantResults[0];
+                response++;
+                response--;
+            }
+        };
         switch(requestCode) {
             case PERMISSION_LOCATION_REQUEST_CODE:
                 if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
